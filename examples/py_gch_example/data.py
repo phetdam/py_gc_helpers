d0c80add74c08d077359c7c20e8148bc55cb7248
Derek Huang <djh458@stern.nyu.edu>
2021-01-30 02:13:18 -0500

    add make_linear_regression in data.py to help with Adam testing + re-add ext_example.c to the repo

---
+++ examples/py_gch_example/data.py
@@ -0,0 +1,62 @@
+__doc__ = "Utilities for data set generation."
+
+from functools import partial
+import numpy as np
+
+
+def make_linear_regression(
+    n_train = 800, n_test = 200, n_features = 10, feature_cov = None,
+    output_mean = 0, output_std = 1, rng = None
+):
+    """Returns data suitable for fitting a linear regression model.
+
+    Input features are centered (zero-mean); their covariance may be specified
+    with a covariance matrix. The output mean (model bias) and standard
+    deviation of Gaussian errors of the output may also be specified.
+
+    The "true" parameters of the model are drawn uniformly and independently
+    from the unit interval.
+
+    :param n_train: Number of training samples to generate
+    :type n_train: int, optional
+    :param n_test: Number of test samples to generate
+    :type n_test: int, optional
+    :param n_features: Input dimensionality
+    :type n_features: int, optional
+    :param feature_cov: Optional covariance matrix for the input features. If
+        not provided, the identity matrix will be used. If a float is provided,
+        then ``feature_cov`` times the identity matrix will be used.
+    :type feature_cov: float or :class:`numpy.ndarray`, optional
+    :param output_mean: Mean of the regression output. Since inputs are
+        centered, it is identical to the true bias/intercept coefficient term
+        in a linear regression model.
+    :type output_mean: float, optional
+    :param output_std: Standard deviation of the Gaussian regression errors.
+    :type output_std: float, optional
+    :param rng: Int seed for the new ``numpy`` RNG or an existing instance
+    :type rng: int or :class:`numpy.random._generator.Generator`, optional
+    """
+    # set defaults
+    if feature_cov is None:
+        feature_cov = np.eye(n_features)
+    elif isinstance(feature_cov, (int, float)):
+        feature_cov = feature_cov * np.eye(n_features)
+    if rng is None:
+        rng = np.random.default_rng()
+    elif isinstance(rng, int):
+        rng = np.random.default_rng(rng)
+    # get "true" parameters
+    weights = rng.random(size = n_features)
+    # callable to generate input examples. we pass check_valid = "raise" so
+    # that an exception is raised if feature_cov is not positive semidefinite
+    make_inputs = partial(
+        rng.multivariate_normal, np.zeros(n_features), feature_cov,
+        check_valid = "raise"
+    )
+    # callable to generate additive noise for outputs
+    output_noise = partial(rng.normal, scale = output_std)
+    # make training and test data + return
+    X_train, X_test = make_inputs(size = n_train), make_inputs(size = n_test)
+    y_train = X_train @ weights + output_mean + output_noise(size = n_train)
+    y_test = X_test @ weights + output_mean + output_noise(size = n_test)
+    return X_train, X_test, y_train, y_test
---
+++ examples/py_gch_example/ext_example.c
@@ -0,0 +1,294 @@
+/**
+ * @file ext_example.c
+ * @brief An example C extension module using the API provided by `py_gch.h`.
+ */
+
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+
+#include <float.h>
+#include <stdlib.h>
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define PY_ARRAY_UNIQUE_SYMBOL EXT_EXAMPLE_ARRAY_API
+// arrayobject.h gives access to the array API, npymath.h the core math library
+#include "numpy/arrayobject.h"
+#include "numpy/npy_math.h"
+
+#include "py_gch.h"
+
+PyDoc_STRVAR(
+  module_doc,
+  "An example C extension module using the API provided by ``py_gch.h``."
+  "\n\n"
+  "Contains a C implementation of `Kingma and Ba's [#]_ Adam optimizer."
+  "\n\n"
+  ".. [#] Kingma, D.P. & Ba, J. (2017). Adam: A method for stochastic\n"
+  "   *arXiv*. https://arxiv.org/pdf/1412.6980.pdf"
+);
+
+PyDoc_STRVAR(
+  adam_optimizer_doc,
+  "adam_optimizer(obj, grad, x0, args=(), kwargs=None, max_iter=200, "
+  "n_iter_no_change=10, tol=1e-4, alpha=0.001, beta_1=0.9, beta_2=0.999, "
+  "eps=1e-8, disable_gc=True)\n--\n\n"
+  "A bare-bones implementation of Kingma and Ba's Adam optimizer [#]_."
+  "\n\n"
+  "Optimizer parameter defaults are the same as specified in the paper."
+  "\n\n"
+  ".. [#] Kingma, D.P. & Ba, J. (2017). Adam: A method for stochastic\n"
+  "   *arXiv*. https://arxiv.org/pdf/1412.6980.pdf"
+  "\n\n"
+  ":param obj: Objective function to minimize. Must have signature\n"
+  "    ``obj(x, *args, **kwargs)``. ``x`` must be a :class:`numpy.ndarray`\n"
+  "    and the objective must return a scalar.\n"
+  ":type obj: callable\n"
+  ":param grad: Gradient of the objective, with signature\n"
+  "    ``grad(x, *args, **kwargs)``. ``x`` must a :class:`numpy.ndarray` and\n"
+  "    ``grad`` must return a :class:`numpy.ndarray` with the same shape as\n"
+  "    the :class:`numpy.ndarray` ``x``.\n"
+  ":type grad: callable\n"
+  ":param x0: Initial guess for the parameter :class:`numpy.ndarray`\n"
+  ":type x0: :class:`numpy.ndarray`"
+  ":param args: Shared positional args to pass to ``obj``, ``grad``\n"
+  ":type args: tuple, optional\n"
+  ":param kwargs: Shared keyword args to pass to ``obj``, ``grad``\n"
+  ":type kwargs: dict, optional\n"
+  ":param max_iter: Maximum number of iterations to run before termination\n"
+  ":type max_iter: int, optional\n"
+  ":param n_iter_no_change: Number of iterations that objective function\n"
+  "    value doesn't decrease by at least ``tol`` to trigger early stopping\n"
+  ":type n_iter_no_change: int, optional\n"
+  ":param tol: Minimum amount of objective value decrease between\n"
+  "    iterations required to prevent triggering early stopping\n"
+  ":type tol: float, optional\n"
+  ":param alpha: The :math:`\\alpha` parameter for the Adam algorithm that\n"
+  "    gives the stepsize. Must be positive."
+  ":param alpha: float, optional\n"
+  ":param beta_1: The :math:`\\beta_1` parameter for the Adam algorithm that\n"
+  "    controls the decay of the first moment estimate. Must be a value in\n"
+  "    :math:`[0, 1)`.\n"
+  ":type beta_1: float, optional\n"
+  ":param beta_2: The :math:`\\beta_2` parameter for the Adam algorithm that\n"
+  "    controls the decay of the second uncentered moment estimate. Must be\n"
+  "    a value in :math:`[0, 1)`.\n"
+  ":type beta_2: float, optional\n"
+  ":param eps: Positive fudge factor to prevent division by zero when\n"
+  "    computing descent direction\n"
+  ":type eps: float, optional\n"
+  ":param disable_gc: ``True`` to disable garbage collection during\n"
+  "    execution.\n"
+  ":type disable_gc: bool, optional"
+  ":returns: A new :class:`numpy.ndarray` giving an estimate for the\n"
+  "    parameter ``x`` in the objective function.\n"
+  ":rtype: :class:`numpy.ndarray`"
+);
+// Python argument names for adam_optimizer
+static char *adam_optimizer_argnames[] = {
+  "obj", "grad", "x0", "args", "kwargs", "max_iter", "n_iter_no_change",
+  "tol", "alpha", "beta_1", "beta_2", "eps", "disable_gc", NULL
+};
+/**
+ * A simple implementation of Kingma and Ba's Adam optimization algorithm.
+ * 
+ * Python signature:
+ * 
+ * `adam(obj, grad, x0, args=(), kwargs=None, max_iter=100, alpha=0.001,
+ * beta_1=0.9, beta_2=0.999, eps=1e-8, disable_gc=True)`
+ * 
+ * @param self `PyObject *` self (ignored)
+ * @param args `PyObject *` tuple of positional args
+ * @param kwargs `PyObject *` dict of named args, `NULL` if none provided
+ * @returns `PyArrayObject *` cast to `PyObject *` of parameters, the same
+ *     shape as the original guess.
+ */
+static PyObject *adam_impl(PyObject *self, PyObject *args, PyObject *kwargs) {
+  // objective function, gradient function, initial parameter guess (required)
+  PyObject *obj, *grad;
+  PyArrayObject *x0;
+  /**
+   * args and kwargs passed to both obj and grad. note that f_args should also
+   * include x0 as the first element so that it's easier to pass all the
+   * arguments to PyObject_Call.
+   */
+  PyObject *f_args = NULL, *f_kwargs = NULL;
+  // max iterations, number of iterations objective improvement is less than
+  // tol before early stopping is triggered
+  Py_ssize_t max_iter = 200, n_iter_no_change = 10;
+  // min iter improvement, Adam optimizer hyperparameters
+  double tol = 1e-4, alpha = 0.001, beta_1 = 0.9, beta_2 = 0.999, eps = 1e-8;
+  // whether or not to disable garbage collection
+  int disable_gc = 1;
+  // parse parameters. exception set on error
+  if (
+    !PyArg_ParseTupleAndKeywords(
+      args, kwargs, "OOO!|O!O!nndddddp", adam_optimizer_argnames, &obj,
+      &grad, (PyObject **) &x0, &PyArray_Type, &f_args, &PyTuple_Type,
+      &f_kwargs, &PyDict_Type, &max_iter, &n_iter_no_change, &tol, &alpha,
+      &beta_1, &beta_2, &eps, &disable_gc
+    )
+  ) { return NULL; }
+  // check if obj and grad are callable
+  if (!PyCallable_Check(obj)) {
+    PyErr_SetString(PyExc_TypeError, "obj must be callable");
+    return NULL;
+  }
+  if (!PyCallable_Check(grad)) {
+    PyErr_SetString(PyExc_TypeError, "grad must be callable");
+    return NULL;
+  }
+  // check that max_iter, alpha, eps are valid (must be positive)
+  if (max_iter < 1) {
+    PyErr_SetString(PyExc_ValueError, "max_iter must be positive");
+    return NULL;
+  }
+  if (alpha <= 0) {
+    PyErr_SetString(PyExc_ValueError, "alpha must be positive");
+    return NULL;
+  }
+  if (eps <= 0) {
+    PyErr_SetString(PyExc_ValueError, "eps must be positive");
+    return NULL;
+  }
+  // check that n_iter_no_change, tol is valid (must be nonnegative)
+  if (n_iter_no_change < 0) {
+    PyErr_SetString(
+      PyExc_ValueError, "n_iter_no_change must be nonnegative\n"
+    );
+    return NULL;
+  }
+  if (tol < 0) {
+    PyErr_SetString(PyExc_ValueError, "tol must be nonnegative");
+    return NULL;
+  }
+  // warn if eps is too big
+  if (eps >= 1e-1) { 
+    if (
+      PyErr_WarnEx(
+        PyExc_UserWarning, "eps excees 1e-1; step sizes may be overly "
+        "deflated. Consider passing a smaller value.", 1
+      ) < 0
+    ) { return NULL; }
+  }
+  // beta_1, beta_2 must be within [0, 1)
+  if ((beta_1 < 0) || (beta_1 >= 1)) {
+    PyErr_SetString(PyExc_ValueError, "beta_1 must be inside [0, 1)");
+    return NULL;
+  }
+  if ((beta_2 < 0) || (beta_2 >= 2)) {
+    PyErr_SetString(PyExc_ValueError, "beta_2 must be inside [0, 1)");
+    return NULL;
+  }
+  // check that x0 is either int or double array
+  if (!PyArray_ISINTEGER(x0) && !PyArray_ISFLOAT(x0)) {
+    PyErr_SetString(PyExc_TypeError, "x0 must contain either ints or floats");
+    return NULL;
+  }
+  // check if array can be cast to NPY_DOUBLE safely
+  if (!PyArray_CanCastSafely(PyArray_TYPE(x0), NPY_DOUBLE)) {
+    if (
+      PyErr_WarnEx(
+        PyExc_UserWarning, "x0 cannot be safely cast to NPY_DOUBLE. precision "
+        "may be lost during computation", 1
+      ) < 0
+    ) { return NULL; }
+  }
+  /**
+   * create new output array containing NPY_DOUBLE values of x0. a reference
+   * to the PyArrayDescr * is stolen by this function. NPY_ARRAY_CARRAY flags
+   * equals NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED | NPY_ARRAY_WRITEABLE.
+   */
+  PyArrayObject *params = (PyArrayObject *) PyArray_FromArray(
+    x0, PyArray_DescrFromType(NPY_DOUBLE), NPY_ARRAY_CARRAY
+  );
+  // nothing to do on error (all other refs are borrowed)
+  if (params == NULL) {
+    return NULL;
+  }
+  // pack all positional args for obj, grad (params, f_args) into a new tuple.
+  // if f_args is NULL, then there aren't any other positional args to add.
+  Py_ssize_t n_pos_f_args = 1;
+  // we know f_args points to a tuple, so no need to check
+  if (f_args != NULL) {
+    n_pos_f_args = n_pos_f_args + PyTuple_GET_SIZE(f_args);
+  }
+  // new pointer to original f_args (temporary var)
+  PyObject *xf_args = f_args;
+  // new f_args (ignore dropping of borrowed ref)
+  f_args = PyTuple_New(n_pos_f_args);
+  // on error, we need to Py_DECREF params, which is a new ref
+  if (f_args == NULL) {
+    Py_DECREF(params);
+    return NULL;
+  }
+  // fill f_args with params and elements of xf_args (Py_INCREF all of them)
+  Py_INCREF(params);
+  PyTuple_SET_ITEM(f_args, 0, (PyObject *) params);
+  if (xf_args != NULL) {
+    for (Py_ssize_t i = 0; i < PyTuple_GET_SIZE(xf_args); i++) {
+      // all items in xf_args need to get Py_INCREF'd
+      PyObject *xf_args_i = PyTuple_GET_ITEM(xf_args, i);
+      Py_INCREF(xf_args_i);
+      PyTuple_SET_ITEM(f_args, i + 1, xf_args_i);
+    }
+  }
+  /*
+  // evaluate objective function at current guess
+  PyObject *obj_val_ = PyObject_Call(obj, f_args, f_kwargs);
+  // on error, need to Py_DECREF params, f_args (new references)
+  if (obj_val_ == NULL) {
+    Py_DECREF(params);
+    Py_DECREF(f_args);
+    return NULL;
+  }
+  // objective function value
+  double obj_val = PyFloat_AsDouble(obj_val_);
+  // Py_DECFEF params, f_args (new refs on error)
+  if (PyErr_Occurred()) {
+    Py_DECREF(params);
+    Py_DECREF(f_args);
+    return NULL;
+  }
+  // improvement from last iteration
+  double obj_imp = DBL_MAX;
+  // consecutive iterations where obj value has not improved by at least tol
+  Py_ssize_t n_no_change = 0;
+  // number of iterations run - 1
+  Py_ssize_t iter_i = 0;
+  // while not converged
+  while ((iter_i < max_iter) && (n_no_change < n_iter_no_change)) {
+    iter_i++;
+  }
+  */
+  // Py_DECREF f_args
+  Py_DECREF(f_args);
+  // return final parameters
+  return (PyObject *) params;
+}
+
+// method table
+static PyMethodDef mod_methods[] = {
+  {
+    "adam_optimizer",
+    (PyCFunction) adam_impl,
+    METH_VARARGS | METH_KEYWORDS,
+    adam_optimizer_doc  
+  },
+  {NULL, NULL, 0, NULL}
+};
+
+// static module definition
+static PyModuleDef mod_struct = {
+  PyModuleDef_HEAD_INIT,
+  "ext_example",
+  module_doc,
+  -1,
+  mod_methods
+};
+
+PyMODINIT_FUNC PyInit_ext_example(void) {
+  // sets error indicator and returns NULL on error automatically
+  import_array();
+  // create module; if NULL, error
+  return PyModule_Create(&mod_struct);
+}
