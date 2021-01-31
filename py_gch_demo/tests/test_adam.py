4188d2fb2f6fbca962d9201698f91fc3111424c4
Derek Huang <djh458@stern.nyu.edu>
2021-01-31 05:50:46 -0500

    pass sanity checks for adam_optimizer from solvers.c

--- py_gch_demo/tests/test_adam.py
+++ py_gch_demo/tests/test_adam.py
@@ -7,12 +7,33 @@ import pytest
 from ..solvers import adam_optimizer
 
 
-@pytest.mark.skip(reason = "not implemented")
-def test_adam_optimizer_sanity(svm_data):
+def test_adam_optimizer_sanity(linsvm, linsvm_x0, svm_data):
     """Sanity check for ``adam_optimizer`` inputs.
 
+    :param linsvm: ``pytest`` fixture. See package ``conftest.py``.
+    :type linsvm: :class:`~py_gch_demo.models.PrimalLinearSVC`
+    :param linsvm_x0: ``pytest`` fixture. See package ``conftest.py``.
+    :type linsvm_x0: :class:`numpy.ndarray`
     :param svm_data: ``pytest`` fixture. See package ``conftest.py``.
     :type svm_data: tuple
     """
-    # unpack data
-    X_train, X_test, y_train, y_test = svm_data
+    # unpack data and make reference to objective and gradient functions
+    X, _, y, _ = svm_data
+    obj, grad = linsvm._obj_func, linsvm._obj_grad
+    # positional args that should be passed to a correct adam_optimizer call
+    req_args = obj, grad, linsvm_x0, (X, y)
+    # obj, grad must be callable
+    with pytest.raises(TypeError, match = "obj must be callable"):
+        adam_optimizer(None, grad, linsvm_x0, args = (X, y))
+    with pytest.raises(TypeError, match = "grad must be callable"):
+        adam_optimizer(obj, None, linsvm_x0, args = (X, y))
+    # max_iter must be an int and must be positive
+    with pytest.raises(TypeError):
+        adam_optimizer(*req_args, max_iter = "oof")
+    with pytest.raises(ValueError, match = "max_iter must be positive"):
+        adam_optimizer(*req_args, max_iter = 0)
+    # alpha must be float and must be positive
+    with pytest.raises(TypeError):
+        adam_optimizer(*req_args, alpha = "oowee")
+    with pytest.raises(ValueError, match = "alpha must be positive"):
+        adam_optimizer(*req_args, alpha = 0)
