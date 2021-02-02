4e7f81552d254fa0726b218e26912141fce497cd
Derek Huang <djh458@stern.nyu.edu>
2021-02-02 06:37:37 -0500

    update py_gch_suite.c maros to gc module pointer

--- check/py_gch_suite.c
+++ check/py_gch_suite.c
@@ -15,7 +15,8 @@
 #include "check_helpers.h"
 
 // macros that make the debugging process easier
-#define PyGCH_mod_o ((PyObject *) PYGCH_API_UNIQ_SYMBOL[0])
+#define gc_module_ PYGCH_API_UNIQ_SYMBOL[0]
+#define gc_module ((PyObject *) gc_module_)
 
 /**
  * Test that `PyGCH_unique_import` really imports `gc` once.
@@ -24,18 +25,18 @@
  *     places where `Py_DECREF` is necessary.
  */
 PY_C_API_REQUIRED START_TEST(test_unique_import) {
-  // set PyGCH_mod_o to NULL so this test is reproducible. we can't use the
-  // macro because there's a PyObject * cast in the way.
-  PYGCH_API_UNIQ_SYMBOL[0] = NULL;
+  // set gc_module to NULL so this test is reproducible. we need the separate
+  // gc_module_ macro since the cast in gc_module doesn't let us write.
+  gc_module_ = NULL;
   // run unique import. it should return true
   ck_assert_msg(PyGCH_gc_unique_import(), "PyGCH_unique_import failed (1)");
   // current reference count to the gc module (isn't necessarily 1)
-  Py_ssize_t gc_refcnt = Py_REFCNT(PyGCH_mod_o);
+  Py_ssize_t gc_refcnt = Py_REFCNT(gc_module);
   // run again to see if the reference count changes
   ck_assert_msg(PyGCH_gc_unique_import(), "PyGCH_unique_import failed (2)");
   // reference count should not have changed. if it has, we should do cleanup,
   // but that's ok since the interpreter is going to be finalized anyways.
-  ck_assert_double_eq(Py_REFCNT(PyGCH_mod_o), gc_refcnt);
+  ck_assert_double_eq(Py_REFCNT(gc_module), gc_refcnt);
 } END_TEST
 
 /**
